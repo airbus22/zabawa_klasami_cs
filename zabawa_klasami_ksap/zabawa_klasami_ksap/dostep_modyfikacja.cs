db9e6cd266c1ed862314e808529428b34155cec1
airbus22 <airbus22@tlen.pl>
2017-08-02 10:00:27 +0200

    tylko master - czemu nie origin również

---
+++ zabawa_klasami_ksap/zabawa_klasami_ksap/dostep_modyfikacja.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zabawa_klasami_ksap
+{
+    public class Dostep_modyfikacja
+    {
+        public string imie { get; set; }
+        public string nazwisko { get; set; }
+        public int wiek { get; set; }
+        public string uwagi { get; set; }
+    }
+}
