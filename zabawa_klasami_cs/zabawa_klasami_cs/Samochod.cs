ccaa70a632c570e5e89dfe18f14d79f9480f3b67
GB-praca <airbus22@tlen.pl>
2017-08-02 11:21:19 +0200

    zmiany testowe w nowym repozytorium

---
+++ zabawa_klasami_cs/zabawa_klasami_cs/Samochod.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zabawa_klasami_cs
+{
+    class Samochod
+    {
+        public string typ { get; set; }
+        public string marka { get; set; }
+        public string kolor { get; set; }
+        public string wiek { get; set; }
+        public string uwagi { get; set; }
+    }
+}
