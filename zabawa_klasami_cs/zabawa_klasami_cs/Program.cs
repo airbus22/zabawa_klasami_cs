27a91bd526b936ae0d137cb9bed9d0301645765a
GB-praca <airbus22@tlen.pl>
2017-08-04 14:13:33 +0200

    Dodano szkielet kolejnej metody

--- zabawa_klasami_cs/zabawa_klasami_cs/Program.cs
+++ zabawa_klasami_cs/zabawa_klasami_cs/Program.cs
@@ -8,16 +8,16 @@ namespace zabawa_klasami_cs
 {
     public class Program
     {
+        Samochod Osobowy = new Samochod();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Kolor jest: ");
-            
+            Console.WriteLine("Kolor jest: ");            
             Console.ReadKey();
         }
 
         public void test_wypisu(string t, string m, string k, int s, string p, string poj, string w, string u)
-        {
-            Samochod Osobowy = new Samochod();
+        {            
             Osobowy.typ = t;
             Osobowy.marka = m;
             Osobowy.kolor = k;
@@ -26,6 +26,11 @@ namespace zabawa_klasami_cs
             Osobowy.pojemnosc = poj;
             Osobowy.wiek = w;
             Osobowy.uwagi = u;
+        }
+
+        public void wyswietNaEkran()
+        {
+
         }
     }
 }
