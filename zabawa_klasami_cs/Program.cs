c69dd254b64d7187561c1a099aa5a8ea85d11389
GB-praca <airbus22@tlen.pl>
2017-08-16 14:23:06 +0200

    Pelna wersja przykladu

--- zabawa_klasami_cs/Program.cs
+++ zabawa_klasami_cs/Program.cs
@@ -13,12 +13,13 @@ namespace zabawa_klasami_cs
         static void Main(string[] args)
         {
             Samochod gbCar = new Samochod();
+            //Samochod.gbCar = new Samochod();
             gbCar.Kolor = "Zielony";
             gbCar.Marka = "Trabant";
             gbCar.Model = "601s";
             
-            //Console.WriteLine("Podaj rodzaj paliwa: ");
-            //gbCar.Paliwo = Console.ReadLine();
+            Console.WriteLine("Podaj rodzaj paliwa: ");
+            gbCar.Paliwo = Console.ReadLine();
 
             Console.WriteLine("Podaj pojemność: ");
             gbCar.Pojemnosc = double.Parse(Console.ReadLine());
@@ -26,21 +27,61 @@ namespace zabawa_klasami_cs
             Console.WriteLine("Podaj rok produkcji: ");
             gbCar.RokProdukcji = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Kolor: " + gbCar.Kolor);
-            Console.WriteLine("Marka: " + gbCar.Marka);
+            Console.WriteLine("Silnik: ");
+            gbCar.Silnik = Console.ReadLine();
+
+            Console.WriteLine("Typ: ");
+            gbCar.Typ = Console.ReadLine();
+
+            Console.WriteLine("Uwagi: ");
+            gbCar.Uwagi = Console.ReadLine();
+
+            Console.Clear();
+
+            if (gbCar.Model != null)
+                Console.WriteLine("Kolor: " + gbCar.Kolor);
+            else
+                Console.WriteLine("Kolor: nie zdefiniowano");
+
+            if (gbCar.Model != null)
+                Console.WriteLine("Marka: " + gbCar.Marka);
+            else
+                Console.WriteLine("Marka: nie zdefiniowano");
 
             if (gbCar.Model != null)
                 Console.WriteLine("Model: " + gbCar.Model);
             else
-                Console.WriteLine("nie zdefiniowano");
+                Console.WriteLine("Model: nie zdefiniowano");
 
-            Console.WriteLine("Paliwo: " + gbCar.Paliwo);
-            Console.WriteLine("Pojemność: " + gbCar.Pojemnosc);
-            Console.WriteLine("Rok produkcji: " + gbCar.RokProdukcji);
-            Console.WriteLine("Silnik: " + gbCar.Silnik);
-            Console.WriteLine("Typ: " + gbCar.Typ);
-            Console.WriteLine("Uwagi: " + gbCar.Uwagi);
+            if (gbCar.Paliwo != null)
+                Console.WriteLine("Paliwo: " + gbCar.Paliwo);
+            else
+                Console.WriteLine("Paliwo: nie zdefiniowano");
 
+            if (gbCar.Pojemnosc != 0)
+                Console.WriteLine("Pojemność: " + gbCar.Pojemnosc);
+            else
+                Console.WriteLine("Pojemnosc: nie zdefiniowano");
+
+            if (gbCar.RokProdukcji != 0)
+                Console.WriteLine("Rok produkcji: " + gbCar.RokProdukcji);
+            else
+                Console.WriteLine("Rok produkcji: nie zdefiniowano");
+
+            if (gbCar.Silnik != null)
+                Console.WriteLine("Silnik: " + gbCar.Silnik);
+            else
+                Console.WriteLine("Silnik: nie zdefiniowano");
+
+            if (gbCar.Typ != null)
+                Console.WriteLine("Typ: " + gbCar.Typ);
+            else
+                Console.WriteLine("Typ: nie zdefiniowano");
+
+            if (gbCar.Uwagi != null)
+                Console.WriteLine("Uwagi: " + gbCar.Uwagi);
+            else
+                Console.WriteLine("Uwagi: nie zdefiniowano");
             Console.ReadKey();            
         }
 
@@ -60,7 +101,7 @@ namespace zabawa_klasami_cs
         {
             Osobowy.Typ = "Hatchback";
             Osobowy.Marka = "Toyota";
-            //Osobowy.Model = "Auris";
+            Osobowy.Model = "Auris";
             Osobowy.Kolor = "grafitowy";
             Osobowy.Silnik = "V4 8V";
             Osobowy.Paliwo = "Olej napędowy";
