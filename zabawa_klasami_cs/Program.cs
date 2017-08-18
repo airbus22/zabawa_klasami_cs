ef6aa124bc09c4dfb33a56be4052c7f51471e28e
GB-dell <grzegorz.brodowski@outlook.com>
2017-08-18 18:52:52 +0200

    Zakomentowano dwie niuzywane metody

--- zabawa_klasami_cs/Program.cs
+++ zabawa_klasami_cs/Program.cs
@@ -143,32 +143,32 @@ namespace zabawa_klasami_cs
         //    Osobowy.uwagi = u;
         //}
 
-        public void Test_wypisu()
-        {
-            Osobowy.Typ = "Hatchback";
-            Osobowy.Marka = "Toyota";
-            Osobowy.Model = "Auris";
-            Osobowy.Kolor = "grafitowy";
-            Osobowy.Silnik = "V4 8V";
-            Osobowy.Paliwo = "Olej napędowy";
-            Osobowy.Pojemnosc = 1.4;
-            Osobowy.RokProdukcji = 2011;
-            Osobowy.Uwagi = "super wóz - uwag brak";
-        }
+        //public void Test_wypisu()
+        //{
+        //    Osobowy.Typ = "Hatchback";
+        //    Osobowy.Marka = "Toyota";
+        //    Osobowy.Model = "Auris";
+        //    Osobowy.Kolor = "grafitowy";
+        //    Osobowy.Silnik = "V4 8V";
+        //    Osobowy.Paliwo = "Olej napędowy";
+        //    Osobowy.Pojemnosc = 1.4;
+        //    Osobowy.RokProdukcji = 2011;
+        //    Osobowy.Uwagi = "super wóz - uwag brak";
+        //}
 
-        public void WyswietNaEkran()
-        {
-            Test_wypisu();
-            Console.WriteLine("Wyniki wyszukiwania:");
-            Console.WriteLine(Osobowy.Typ.ToString());
-            Console.WriteLine(Osobowy.Marka.ToString());
-            Console.WriteLine(Osobowy.Model.ToString());
-            Console.WriteLine(Osobowy.Kolor.ToString());
-            Console.WriteLine(Osobowy.Silnik.ToString());
-            Console.WriteLine(Osobowy.Paliwo.ToString());
-            Console.WriteLine(Osobowy.Pojemnosc.ToString());
-            Console.WriteLine(Osobowy.RokProdukcji.ToString());
-            Console.WriteLine(Osobowy.Uwagi.ToString());
-        }
+        //public void WyswietNaEkran()
+        //{
+        //    Test_wypisu();
+        //    Console.WriteLine("Wyniki wyszukiwania:");
+        //    Console.WriteLine(Osobowy.Typ.ToString());
+        //    Console.WriteLine(Osobowy.Marka.ToString());
+        //    Console.WriteLine(Osobowy.Model.ToString());
+        //    Console.WriteLine(Osobowy.Kolor.ToString());
+        //    Console.WriteLine(Osobowy.Silnik.ToString());
+        //    Console.WriteLine(Osobowy.Paliwo.ToString());
+        //    Console.WriteLine(Osobowy.Pojemnosc.ToString());
+        //    Console.WriteLine(Osobowy.RokProdukcji.ToString());
+        //    Console.WriteLine(Osobowy.Uwagi.ToString());
+        //}
     }
 }
