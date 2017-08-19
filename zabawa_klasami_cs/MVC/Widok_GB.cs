85d2ff543dc66bf41508731d8d2a0b2c80408998
GB-Radom <grzegorz.brodowski@outlook.com>
2017-08-19 21:30:06 +0200

    nowa struktura dla wzorca MVC

---
+++ zabawa_klasami_cs/MVC/Widok_GB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zabawa_klasami_cs.MVC
+{
+    class Widok_GB
+    {
+    }
+}
